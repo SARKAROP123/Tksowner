f267c82cb74391d16568da5bb480ba052aed7543
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2023-07-14 10:44:16 +0530

    Update play.py
    Signed-off-by: SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>

--- AnonX/utils/inline/play.py
+++ AnonX/utils/inline/play.py
@@ -61,7 +61,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="✯ ᴄʟᴏsᴇ ✯", callback_data=f"close"
+                text="❌ 𝑪𝑳𝑶𝑺𝑬 ❌", callback_data=f"close"
             )
         ],
     ]
@@ -118,7 +118,7 @@ def telegram_markup_timer(_, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="✯ ᴄʟᴏsᴇ ✯", callback_data=f"close"
+                text="❌ 𝑪𝑳𝑶𝑺𝑬 ❌", callback_data=f"close"
             )
         ],
     ]
@@ -147,7 +147,7 @@ def stream_markup(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="✯ ᴄʟᴏsᴇ ✯", callback_data=f"close"
+                text="❌ 𝑪𝑳𝑶𝑺𝑬 ❌", callback_data=f"close"
             )
         ],
     ]
@@ -173,7 +173,7 @@ def telegram_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="✯ ᴄʟᴏsᴇ ✯", callback_data=f"close"
+                text="❌ 𝑪𝑳𝑶𝑺𝑬 ❌", callback_data=f"close"
             )
         ],
     ]
@@ -297,7 +297,7 @@ close_keyboard = InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        text="✯ ᴄʟᴏsᴇ ✯", callback_data="close"
+                        text="❌ 𝑪𝑳𝑶𝑺𝑬 ❌", callback_data="close"
                     )
                 ]    
             ]
@@ -328,7 +328,7 @@ def queue_markup(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="✯ ᴄʟᴏsᴇ ✯", callback_data=f"close"
+                text="❌ 𝑪𝑳𝑶𝑺𝑬 ❌", callback_data=f"close"
             )
         ],
     ]
