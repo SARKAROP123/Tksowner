5c844016f00555dbeb54049da3bf7a9545d88695
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2023-09-30 20:08:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SARKAROP123/AnonXMusi",
+    "https://github.com/SARKAROP123/Tksowner",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
